33030477852724f2c302f63170ae8cdc994fd8e2
Lembou Pharel <lemboupharel@proton.me>
2025-08-25 19:58:46 +0100

    set express main endpoint, static file and frontend serving logic

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const serverApp = express();
+const PORT = process.env.PORT || 5000  // port number here
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// middle ware
+    // serving static files
+    serverApp.use(express.static(path.join(__dirname, './../public')));
+
+    // accept Json data
+    app.use(express.json());
+
+// end points
+serverApp.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './../views', 'login.html'));
+});
+
+
+serverApp.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+})
